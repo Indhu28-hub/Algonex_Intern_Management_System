9df680c5e35bbf977a667192dc18993a424d2a09
Indhu28 <indhu28.hub@gmail.com>
2026-06-04 20:06:29 +0530

    Add Priya's Cafe System mini project

---
+++ mini_project.py/mini_project3.py
@@ -0,0 +1,30 @@
+'''Priya's Caffe System
+1. order: 'mocha','tea','Green tea','cold coffee', 'Latte', 'Cappuccino', 'Espresso',
+2. size: 'small-240', 'medium-320', 'large-360'
+3. price: 'mocha'- 150, 'tea'- 100, 'Green tea'- 120, 'cold coffee'- 130, 'Latte'- 140, 'Cappuccino'- 160, 'Espresso'- 170
+4. quantity: input from user
+5. add-on: biscuit, cookies
+6. Total Discount: 15% if total price is above 700
+7. total price: price * quantity
+'''
+
+menu = {
+    'mocha': 150,
+    'tea': 100,
+    'Green tea': 120,
+    'cold coffee': 130,
+    'Latte': 140,
+    'Cappuccino': 160,
+    'Espresso': 170
+}
+sizes = {
+    'small': 240,
+    'medium': 320,
+    'large': 360
+}
+add_ons = {
+    'biscuit': 20,
+    'cookies': 30
+}
+def 
+
